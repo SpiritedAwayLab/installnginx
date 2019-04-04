f8f5526f4233814d3473e4d75fc45ca67036271c
SpiritedAwayLab <35682566+SpiritedAwayLab@users.noreply.github.com>
2019-04-04 03:31:47 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 apt-get update
 apt-get install build-essential libssl-dev -y
-wget ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.42.tar.gz
+curl ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.42.tar.gz -o pcre-8.42.tar.gz
 tar -zxf pcre-8.42.tar.gz
 cd pcre-8.42
 ./configure
@@ -8,7 +8,7 @@ make
 make install
 
 cd
-wget http://zlib.net/zlib-1.2.11.tar.gz
+curl http://zlib.net/zlib-1.2.11.tar.gz -o zlib-1.2.11.tar.gz
 tar -zxf zlib-1.2.11.tar.gz
 cd zlib-1.2.11
 ./configure
@@ -16,7 +16,7 @@ make
 make install
 
 cd
-wget http://www.openssl.org/source/openssl-1.1.1b.tar.gz
+curl http://www.openssl.org/source/openssl-1.1.1b.tar.gz -o openssl-1.1.1b.tar.gz
 tar -zxf openssl-1.1.1b.tar.gz
 cd openssl-1.1.1b
 ./Configure darwin64-x86_64-cc --prefix=/usr
@@ -24,7 +24,7 @@ make
 make install
 
 cd
-wget https://nginx.org/download/nginx-1.14.2.tar.gz
+curl https://nginx.org/download/nginx-1.14.2.tar.gz -o nginx-1.14.2.tar.gz
 tar zxf nginx-1.14.2.tar.gz
 cd nginx-1.14.2
 ./configure --sbin-path=/usr/local/nginx/nginx --conf-path=/usr/local/nginx/nginx.conf --pid-path=/usr/local/nginx/nginx.pid --with-pcre=../pcre-8.42 --with-zlib=../zlib-1.2.11  --with-stream  
